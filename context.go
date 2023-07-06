b7ac3ef5f16c713c6b318f797445b17be7498a03
Lexographics <zbamgamesinfo@gmail.com>
2023-07-06 21:59:38 +0300

    Add send file

--- context.go
+++ context.go
@@ -1,6 +1,11 @@
 package dcroute
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"bufio"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 type Context struct {
 	Sender    *User
@@ -13,5 +18,16 @@ type Context struct {
 
 func (c *Context) SendMessage(channel string, m string) error {
 	_, err := c.session.ChannelMessageSend(channel, m)
+	return err
+}
+
+func (c *Context) SendFile(channel string, name string, filepath string) error {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return err
+	}
+	reader := bufio.NewReader(file)
+
+	_, err = c.session.ChannelFileSend(channel, name, reader)
 	return err
 }
--- example/main.go
+++ example/main.go
@@ -33,6 +33,11 @@ func main() {
 		fmt.Printf("Got message form %s with id %s\n", ctx.Sender.Username, ctx.Sender.ID)
 
 		ctx.SendMessage(ctx.ChannelID, "pong")
+		err := ctx.SendFile(ctx.ChannelID, "image.jpg", "example/image.png")
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+		}
+
 		return nil
 	})
 
